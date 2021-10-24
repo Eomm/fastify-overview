e079445ddec1f6bf6beeeafe0fe7e0b918610735
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-10-24 13:07:07 +0200

    working draft

--- index.js
+++ index.js
@@ -1,5 +1,92 @@
 'use strict'
 
-module.exports = function () {
+const fp = require('fastify-plugin')
+const trackerMe = Symbol('fastify-overview.track-me')
 
+function fastifyOverview (fastify, opts, next) {
+  const contextMap = new Map()
+  let structure
+
+  fastify.addHook('onRegister', function hook (instance, opts) {
+    const parent = Object.getPrototypeOf(instance)
+    manInTheMiddle(instance, parent[trackerMe])
+  })
+
+  // fastify.addHook('onReady', function hook (done) {
+  //   const root = contextMap.get(this)
+  //   structure = root
+  //   contextMap.clear()
+  //   done(null)
+  // })
+
+  fastify.decorate('overview', function getOverview () {
+    if (!structure) {
+      const root = contextMap.get(rootTrack)
+      structure = root
+      contextMap.clear()
+    }
+
+    return structure
+  })
+
+  const rootTrack = manInTheMiddle(fastify)
+
+  next()
+
+  function manInTheMiddle (instance, parentId) {
+    const trackingToken = Math.random()
+    instance[trackerMe] = trackingToken
+
+    const trackStructure = getEmptyTree()
+    if (parentId) {
+      contextMap.get(parentId).children.push(trackStructure)
+    }
+
+    contextMap.set(trackingToken, trackStructure)
+
+    const originalDecorate = instance.decorate
+    // const originalDecorateRequest = instance.decorateRequest // TODO
+    // const originalDecorateReply = instance.decorateReply // TODO
+    instance.decorate = function wrapDecorate (name, value) {
+      contextMap.get(trackingToken).decorators.decorate.push(name)
+      return originalDecorate.call(this, name, value)
+    }
+
+    const originalHook = instance.addHook
+    instance.addHook = function wrapAddHook (name, hook) {
+      contextMap.get(trackingToken).hooks[name].push(hook.toString())
+      return originalHook.call(this, name, hook)
+    }
+
+    return trackingToken
+  }
+}
+
+module.exports = fp(fastifyOverview, {
+  name: 'fastify-overview'
+})
+
+function getEmptyTree (nodeFunction) {
+  return {
+    name: nodeFunction.name,
+    children: [],
+    decorators: {
+      decorate: []
+    },
+    hooks: {
+      onRequest: [],
+      preParsing: [],
+      preValidation: [],
+      preHandler: [],
+      preSerialization: [],
+      onError: [],
+      onSend: [],
+      onResponse: [],
+      onTimeout: [],
+      onReady: [],
+      onClose: [],
+      onRoute: [],
+      onRegister: []
+    }
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,46 @@
 'use strict'
 
 const { test } = require('tap')
-const CHANGE_ME = require('../index')
+const fastify = require('fastify')
+const plugin = require('../index')
 
-test('CHANGE ME', t => {
-  t.plan(1)
-  CHANGE_ME()
-  t.pass('CHANGE ME')
+// test('basic test', async t => {
+//   const app = fastify()
+//   await app.register(plugin)
+
+//   app.decorate('test', function testFunction () {})
+//   app.decorate('testObject', {})
+//   app.decorate('testArray', [])
+
+//   app.addHook('onRequest', function hook1 () {})
+//   app.addHook('preParsing', function hook2 () {})
+//   app.addHook('preValidation', function hook3 () {})
+//   app.addHook('preHandler', function hook4 () {})
+//   app.addHook('preSerialization', function hook5 () {})
+//   app.addHook('onError', function hook6 () {})
+//   app.addHook('onSend', function hook7 () {})
+//   app.addHook('onResponse', function hook8 () {})
+//   app.addHook('onTimeout', function hook9 () {})
+
+//   // const structure = app.overview()
+//   // t.same(structure, {}) // todo
+// })
+
+test('register', async t => {
+  const app = fastify()
+  await app.register(plugin)
+
+  app.register(function register1 (instance, opts, next) {
+    instance.register(function register2 (instance, opts, next) {
+      next()
+    })
+    instance.register(function register3 (instance, opts, next) {
+      next()
+    })
+    next()
+  })
+
+  await app.ready()
+  const structure = app.overview()
+  t.same(structure, {}) // todo
 })
