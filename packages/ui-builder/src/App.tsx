11af95a55e0f5e298389c432aee7380207e0d46a
Raúl Sáez <raulsaezp@gmail.com>
2022-05-03 14:35:12 +0100

    Remove DetailsBar

--- packages/ui-builder/src/App.tsx
+++ packages/ui-builder/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { DataNode } from '../types';
-// import DetailsBar from './components/DetailsBar';
 import Header from './components/Header';
 import RadialTree from './components/RadialTree';
 import useGraphData from './hooks/useGraphData';
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className="flex w-screen h-screen flex-col">
       <Header {...{ filters, setFilters, currentNode }}></Header>
-      {/* <DetailsBar className="w-4/12 lg:w-4/12" /> */}
       <RadialTree
         {...{ chartData, currentNode, setCurrentNode }}
         className="w-full shadow-inner h-full"
