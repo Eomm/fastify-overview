2504d27c3ccda1c66a8fa6254176454f3494ced4
Raúl Sáez <raulsaezp@gmail.com>
2022-05-05 14:01:36 +0100

    Remove UI files

--- packages/ui-builder/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import overviewReducer from './overviewSlice'
-
-export default configureStore({
-  reducer: {
-    overview: overviewReducer
-  }
-})
--- packages/ui-builder/src/utils/strings.js
+++
@@ -1,9 +0,0 @@
-export function compact (str, maxLength) {
-  if (!str) return ''
-  if (str.length <= maxLength) {
-    return str
-  }
-  return (
-    str.substring(0, maxLength / 2) + '...' + str.substring(str.length - maxLength / 2)
-  )
-}
