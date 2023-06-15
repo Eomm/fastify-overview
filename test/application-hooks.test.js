0f9b9a0f98c319c08a14d4941c16596793726b03
Manuel Spigolon <behemoth89@gmail.com>
2023-06-15 08:23:03 +0200

    fix: add missing preClose hook (#72)

---
+++ test/application-hooks.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { test } = require('tap')
+const fastify = require('fastify')
+const plugin = require('../index')
+
+test('should track all the application hooks', {
+  skip: process.platform === 'win32'
+}, async t => {
+  const app = fastify()
+  await app.register(plugin)
+
+  app.addHook('onReady', function onReady (done) {
+    done()
+  })
+  app.addHook('onReady', async function onReadyAsync () { })
+
+  app.addHook('onClose', function onClose (app, done) {
+    done()
+  })
+  app.addHook('onClose', async function onCloseAsync (app) { })
+
+  app.addHook('onRoute', function onRoute (routeOptions) { })
+  app.addHook('onRegister', function onRegister (app, opts) { })
+
+  app.addHook('preClose', function preClose (done) { done() })
+  app.addHook('preClose', async function preCloseAsync () { })
+
+  await app.ready()
+  const structure = app.overview({ hideEmpty: true })
+
+  t.same(structure.hooks, require('./fixture/app-hooks.json').hooks)
+})
